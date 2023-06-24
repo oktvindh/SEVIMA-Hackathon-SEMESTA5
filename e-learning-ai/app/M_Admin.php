9dc68cad19d8db8c3dd7b5a641b117ada1f79db1
oktvindh <indahoktav02@gmail.com>
2023-06-24 11:07:27 +0700

    e-learning-laravel

---
+++ e-learning-ai/app/M_Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class M_Admin extends Model
+{
+    protected $table = 'tbl_user';
+    protected $primaryKey = 'id_user';
+    protected $fillable = ['ide_user', 'nama', 'email', 'password', 'token'];
+
+}
